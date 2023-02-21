0ea79dac0be057522df6a709d3bee7107fb9504f
opilane <opilane@HP-CZC0359NP0>
2023-02-21 09:17:23 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<int, string> koolid = new Dictionary<int, string>();
+
+        koolid.Add(1, "Nõmme gümnaasium");
+        koolid.Add(2, "TTHK");
+        koolid.Add(3, "Rahumäe põhikool");
+        
+
+        foreach (KeyValuePair<int, string> kool in koolid)
+        {
+            Console.WriteLine("kooli number {0} nimi on {1}.", kool.Key, kool.Value);
+        }
+
+        Console.ReadLine();
+    }
+}
